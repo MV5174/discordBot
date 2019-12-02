375405fe98ae05c4de1ef57645ab59352e539fa4
Michael Vega <ichigo509@yahoo.com>
2019-12-02 00:55:15 -0500

    deploying bot after refactoring

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = (client, member) => {
+    member.send(
+        `Welcome to the server! Don't be a dick, have fun!`
+    )
+  }
--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const fs = require('fs')
 require('dotenv').config()
 
+//reads the files and fires their eventHandlers
+fs.readdir('./events/', (err, files) => {
+    files.forEach(file => {
+        const eventHandler = require(`./events/${file}`)
+        const eventName = file.split('.')[0]
+        client.on(eventName, arg => eventHandler(client, arg))
+    })
+})
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
+    if (msg.content === 'ping') {
+        msg.reply('Pong!')
+    }
 })
 
 client.on('guildMemberAdd', member => {
     member.send(
-      `Welcome to the server! Don't be a dick, have fun!`
+        `Welcome to the server! Don't be a dick, have fun!`
     )
-  })
+})
 
 client.login(process.env.BOT_TOKEN)

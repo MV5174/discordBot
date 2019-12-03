7cb97d4bcc464f2f6ee7085f60f99410842dd55b
Michael Vega <ichigo509@yahoo.com>
2019-12-02 19:25:14 -0500

    made so checks for permissions for kick

--- commands/kicks.js
+++ commands/kicks.js
@@ -8,9 +8,15 @@ module.exports = message => {
     if (!member.kickable) {
       return message.reply(`I can't kick this user. Sorry!`)
     }
-  
-    return member
+
+    if (message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
+      return member
       .kick()
       .then(() => message.reply(`${member.user.tag} was kicked.`))
       .catch(error => message.reply(`Sorry, an error occured.`))
+    }
+    else {
+      return message.reply(`You don't have the permissions to do so, sorry loser`)
+    }
+    
   }
